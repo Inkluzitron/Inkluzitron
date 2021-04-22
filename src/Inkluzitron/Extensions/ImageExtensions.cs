e002398c391d83d26f3f7ba4de33e76f3fc51771
Michal Halabica <m.halabica@gmail.com>
2021-04-22 20:58:29 +0200

    Implementace příkazu bonk

---
+++ src/Inkluzitron/Extensions/ImageExtensions.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+
+namespace Inkluzitron.Extensions
+{
+    static public class ImageExtensions
+    {
+        static public Image RoundImage(this Image original)
+        {
+            using var brush = new TextureBrush(original);
+
+            var rounded = new Bitmap(original.Width, original.Height, original.PixelFormat);
+            rounded.MakeTransparent();
+
+            using var g = Graphics.FromImage(rounded);
+            using var gp = new GraphicsPath();
+
+            g.Clear(Color.Transparent);
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+
+            gp.AddEllipse(0, 0, original.Width, original.Height);
+            g.FillPath(brush, gp);
+
+            return rounded;
+        }
+
+        /// <summary>
+        /// Resizes image
+        /// </summary>
+        /// <remarks>https://stackoverflow.com/a/24199315</remarks>
+        static public Image ResizeImage(this Image original, int width, int height)
+        {
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(original.HorizontalResolution, original.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBilinear;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using var wrapMode = new ImageAttributes();
+                wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                graphics.DrawImage(original, destRect, 0, 0, original.Width, original.Height, GraphicsUnit.Pixel, wrapMode);
+            }
+
+            return destImage;
+        }
+    }
+}
---
+++ src/Inkluzitron/Modules/ImagesModule.cs
@@ -0,0 +1,92 @@
+﻿using AnimatedGif;
+using Discord;
+using Discord.Commands;
+using Inkluzitron.Extensions;
+using Inkluzitron.Resources.Bonk;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using SysDrawImage = System.Drawing.Image;
+
+namespace Inkluzitron.Modules
+{
+    public class ImagesModule : ModuleBase
+    {
+        public ImagesModule()
+        {
+            if (!Directory.Exists("ImageCache"))
+                Directory.CreateDirectory("ImageCache");
+        }
+
+        #region Bonk
+
+        [Command("bonk")]
+        [Summary("Praští uživatele baseballovou pálkou.")]
+        public async Task BonkAsync(IUser member = null)
+        {
+            if (member == null) member = Context.User;
+            var gifName = CreateCachePath($"bonk_{member.Id}_{member.AvatarId ?? member.Discriminator}.gif");
+
+            if (!File.Exists(gifName))
+            {
+                var profilePicture = await GetProfilePictureAsync(member);
+                using var gif = new AnimatedGifCreator(gifName);
+
+                var frames = GetBitmapsFromResources<BonkResources>();
+                for (int i = 0; i < frames.Count; i++)
+                {
+                    var frame = frames[i];
+                    var bonkFrame = RenderBonkFrame(profilePicture, frame, i);
+
+                    await gif.AddFrameAsync(bonkFrame, quality: GifQuality.Bit8);
+                }
+            }
+
+            await ReplyFileAsync(gifName);
+        }
+
+        static private Bitmap RenderBonkFrame(SysDrawImage profilePicture, Bitmap frame, int index)
+        {
+            var bitmap = new Bitmap(200, 170);
+            bitmap.MakeTransparent();
+
+            var deformation = new[] { 0, 0, 0, 5, 10, 20, 15, 5 };
+            using var frameAvatar = profilePicture.ResizeImage(110, 100 - deformation[index]);
+
+            using var g = Graphics.FromImage(bitmap);
+            g.DrawImage(frameAvatar, 80, 60 + deformation[index]);
+            g.DrawImage(frame, 0, 0);
+
+            return bitmap;
+        }
+
+        #endregion
+
+        #region Common parts
+
+        static private async Task<SysDrawImage> GetProfilePictureAsync(IUser user)
+        {
+            var profilePictureData = await user.DownloadProfilePictureAsync();
+            using var memStream = new MemoryStream(profilePictureData);
+            using var rawProfileImage = SysDrawImage.FromStream(memStream);
+            using var roundedProfileImage = rawProfileImage.RoundImage();
+
+            return roundedProfileImage.ResizeImage(100, 100);
+        }
+
+        static private string CreateCachePath(string filename) => Path.Combine("ImageCache", filename);
+
+        static private List<Bitmap> GetBitmapsFromResources<TResources>()
+        {
+            return typeof(TResources).GetProperties()
+                .Where(o => o.PropertyType == typeof(Bitmap))
+                .Select(o => o.GetValue(null, null) as Bitmap)
+                .Where(o => o != null)
+                .ToList();
+        }
+
+        #endregion
+    }
+}
