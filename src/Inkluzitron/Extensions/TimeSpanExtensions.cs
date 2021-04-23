bf6beaec305e8f54d3abfc6eb73d1d2d4fa40dae
Michal Halabica <m.halabica@gmail.com>
2021-04-23 14:52:54 +0200

    Apply suggestions from code review
    Co-authored-by: Jan Chaloupka <osobni@janchaloupka.cz>

--- src/Inkluzitron/Extensions/TimeSpanExtensions.cs
+++ src/Inkluzitron/Extensions/TimeSpanExtensions.cs
@@ -35,12 +35,12 @@ namespace Inkluzitron.Extensions
 
             if (timeSpan.Minutes > 0 || timeSpan.Seconds > 0)
             {
-                if (timeSpan.Seconds == 0 || timeSpan.Seconds > 4) parts.Add($"{timeSpan.Seconds} vteřin");
-                else if (timeSpan.Seconds == 1) parts.Add("1 vteřinu");
-                else parts.Add($"{timeSpan.Seconds} vteřiny");
+                if (timeSpan.Seconds == 0 || timeSpan.Seconds > 4) parts.Add($"{timeSpan.Seconds} sekund");
+                else if (timeSpan.Seconds == 1) parts.Add("1 sekundu");
+                else parts.Add($"{timeSpan.Seconds} sekundy");
             }
 
-            if (parts.Count == 0) return "0 vteřin";
+            if (parts.Count == 0) return "0 sekund";
             else if (parts.Count == 1) return parts[0];
             else return $"{string.Join(", ", parts.Take(parts.Count - 1))} a {parts[^1]}";
         }
