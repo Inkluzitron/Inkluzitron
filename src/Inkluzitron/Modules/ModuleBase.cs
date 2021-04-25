d29d524de256e4999986204e42987ff84f41c1bc
Michal Halabica <m.halabica@gmail.com>
2021-04-25 22:41:25 +0200

    Reverted commit

--- src/Inkluzitron/Modules/ModuleBase.cs
+++ src/Inkluzitron/Modules/ModuleBase.cs
@@ -1,32 +1,28 @@
 ﻿using Discord;
 using Discord.Commands;
 using Discord.Rest;
-using System.IO;
 using System.Threading.Tasks;
 
 namespace Inkluzitron.Modules
 {
     public class ModuleBase : ModuleBase<SocketCommandContext>
     {
-        protected MessageReference ReplyReference => new MessageReference(Context.Message.Id, Context.Channel.Id, Context.Guild?.Id);
-
         protected Task<IUserMessage> ReplyAsync(string message = null, bool isTTS = false, Embed embed = null, AllowedMentions allowedMentions = null)
         {
+            var options = RequestOptions.Default;
+            var reference = new MessageReference(Context.Message.Id, Context.Channel.Id, Context.Guild.Id);
             allowedMentions = CheckAndFixAllowedMentions(allowedMentions);
-            return base.ReplyAsync(message, isTTS, embed, null, allowedMentions, ReplyReference);
+
+            return base.ReplyAsync(message, isTTS, embed, options, allowedMentions, reference);
         }
 
         protected Task<RestUserMessage> ReplyFileAsync(string filePath, AllowedMentions allowedMentions = null)
         {
+            var options = RequestOptions.Default;
+            var reference = new MessageReference(Context.Message.Id, Context.Channel.Id, Context.Guild.Id);
             allowedMentions = CheckAndFixAllowedMentions(allowedMentions);
 
-            return Context.Channel.SendFileAsync(filePath, options: null, allowedMentions: allowedMentions, messageReference: ReplyReference);
-        }
-
-        protected Task<RestUserMessage> ReplyStreamAsync(Stream stream, string filename, bool spoiler = false, string text = null, bool isTTS = false, Embed embed = null, AllowedMentions allowedMentions = null)
-        {
-            allowedMentions = CheckAndFixAllowedMentions(allowedMentions);
-            return Context.Channel.SendFileAsync(stream, filename, text, isTTS, embed, null, spoiler, allowedMentions, ReplyReference);
+            return Context.Channel.SendFileAsync(filePath, options: options, allowedMentions: allowedMentions, messageReference: reference);
         }
 
         static protected AllowedMentions CheckAndFixAllowedMentions(AllowedMentions allowedMentions)
