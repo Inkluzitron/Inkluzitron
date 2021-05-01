ab72b178e681433d19ed078451e2ead99abf9485
/crackono.sh <31440135+crackonosh@users.noreply.github.com>
2021-05-01 16:20:23 +0200

    Update HuggingModule.cs
    viz #46 add Name() parameter

--- src/Inkluzitron/Modules/HuggingModule.cs
+++ src/Inkluzitron/Modules/HuggingModule.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 
 namespace Inkluzitron.Modules
 {
+  [Name("Hugování")]
   public class HuggingModule : ModuleBase
   {
     private IConfiguration Config { get; }
