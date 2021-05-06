85e82102297908886763de9994ca3a4ffed38add
Michal Halabica <m.halabica@gmail.com>
2021-05-06 18:38:09 +0200

    Oprava unicode emotů

---
+++ src/Inkluzitron/Extensions/EmoteExtensions.cs
@@ -0,0 +1,29 @@
+﻿using DC = Discord;
+using NeoSmart.Unicode;
+using System.Linq;
+
+namespace Inkluzitron.Extensions
+{
+    static public class EmoteExtensions
+    {
+        /// <summary>
+        /// Compares two emotes. Supports unicode.
+        /// </summary>
+        static public bool IsEqual(this DC.IEmote emote, DC.IEmote another)
+        {
+            if (emote.GetType() != another.GetType())
+                return false;
+
+            if(emote is DC.Emoji emoji)
+            {
+                var emojiCodepoint = emoji.Name.Codepoints().FirstOrDefault();
+                var anotherEmojiCodepoint = another.Name.Codepoints().FirstOrDefault();
+
+                return emojiCodepoint == anotherEmojiCodepoint;
+            }
+
+            // In a case of standard emotes.
+            return emote.Equals(another);
+        }
+    }
+}
