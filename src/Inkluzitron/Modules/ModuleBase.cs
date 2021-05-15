db057aed66955354710c1ee627d41091825ceb55
Michal Halabica <m.halabica@gmail.com>
2021-05-15 16:42:41 +0200

    Ošetření dlouhých zpráv v BDSM modulu.

--- src/Inkluzitron/Modules/ModuleBase.cs
+++ src/Inkluzitron/Modules/ModuleBase.cs
@@ -1,6 +1,8 @@
 ﻿using Discord;
 using Discord.Commands;
 using Discord.Rest;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Inkluzitron.Modules
@@ -8,6 +10,25 @@ namespace Inkluzitron.Modules
     public class ModuleBase : ModuleBase<SocketCommandContext>
     {
         private MessageReference ReplyReference => new MessageReference(Context.Message.Id, Context.Channel.Id, Context.Guild?.Id);
+
+        protected async Task<List<IUserMessage>> ReplyAsync(IEnumerable<string> messages, bool isTTS = false, AllowedMentions allowedMentions = null)
+        {
+            if (!messages.Any()) // No messages
+                return new List<IUserMessage>();
+
+            allowedMentions = CheckAndFixAllowedMentions(allowedMentions);
+
+            // Apply message reference (reply) only for first message.
+            var resultMessages = new List<IUserMessage>
+            {
+                await ReplyAsync(messages.First(), isTTS, null, allowedMentions)
+            };
+
+            foreach (var msg in messages.Skip(1))
+                resultMessages.Add(await ReplyAsync(msg, false, null, null, allowedMentions, null));
+
+            return resultMessages;
+        }
 
         protected Task<IUserMessage> ReplyAsync(string message = null, bool isTTS = false, Embed embed = null, AllowedMentions allowedMentions = null)
         {
