dbdf627ec87882a5a04178b184e2208e313ab657
Jan Chaloupka <osobni@janchaloupka.cz>
2021-05-16 23:30:38 +0200

    Odkaz na výsledky testu nastaven na Unique ...teda doufám

---
+++ src/Inkluzitron/Migrations/20210516212903_BdsmTestOrgQuizResultUniqueLink.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Inkluzitron.Migrations
+{
+    public partial class BdsmTestOrgQuizResultUniqueLink : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_QuizResults_Link",
+                table: "QuizResults",
+                column: "Link",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_QuizResults_Link",
+                table: "QuizResults");
+        }
+    }
+}
