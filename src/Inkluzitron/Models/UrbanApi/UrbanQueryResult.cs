ae37f433a35a5430d340b9fc8e809c9a445f5a0b
Jan Chaloupka <osobni@janchaloupka.cz>
2021-05-19 13:53:09 +0200

    Přepracování urban api modelu a vylepšení cache

---
+++ src/Inkluzitron/Models/UrbanApi/UrbanQueryResult.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Inkluzitron.Models.UrbanApi
+{
+    public class UrbanQueryResult
+    {
+        public string Query { get; set; }
+
+        [JsonProperty("list", Required = Required.Always)]
+        public List<UrbanDefinition> Definitions { get; set; }
+    }
+}
