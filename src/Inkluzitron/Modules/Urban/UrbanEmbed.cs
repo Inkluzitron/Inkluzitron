8c4ba57e3bbc28a87f2a6711df95c792b025b780
Jan Chaloupka <osobni@janchaloupka.cz>
2021-05-19 02:20:26 +0200

    Přidání příkazu $urban

---
+++ src/Inkluzitron/Modules/Urban/UrbanEmbed.cs
@@ -0,0 +1,52 @@
+﻿using Discord;
+using Inkluzitron.Extensions;
+using Inkluzitron.Models;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Inkluzitron.Modules.Urban
+{
+    public class UrbanEmbed : EmbedBuilder
+    {
+        private static string CreateLinks(string text)
+        {
+            return Regex.Replace(text,
+                @"\[(.*?)\]",
+                m =>
+                {
+                    var found = m.Groups[1].Value;
+                    return $"[{found}](https://www.urbandictionary.com/define.php?term={HttpUtility.UrlEncode(found)})";
+                });
+        }
+
+        public Task<UrbanEmbed> WithDefinitionAsync(
+            UrbanDefinition definition,
+            string query,
+            string userPicture,
+            int pagesCount,
+            int page = 1)
+        {
+            WithTitle(definition.Word);
+            WithUrl(definition.Permalink);
+            WithAuthor(new EmbedAuthorBuilder()
+            {
+                Name = "Urban Dictionary",
+                Url = "https://www.urbandictionary.com/",
+                IconUrl = userPicture
+            });
+
+            WithTimestamp(definition.WrittenOn);
+            WithFooter($"{page}/{pagesCount} | {definition.Author}");
+            this.WithMetadata(new UrbanEmbedMetadata { PageNumber = page, SearchQuery = query });
+
+            WithDescription(CreateLinks(definition.Definition));
+
+            AddField("Example", CreateLinks(definition.ExampleUsage));
+            AddField("👍", definition.ThumbsUp, inline: true);
+            AddField("👎", definition.ThumbsDown, inline: true);
+
+            return Task.FromResult(this);
+        }
+    }
+}
