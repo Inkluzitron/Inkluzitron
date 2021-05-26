78b5c803fd0101f6e08c9289d8b6f75de9dd810c
Jan Chaloupka <osobni@janchaloupka.cz>
2021-05-26 22:04:21 +0200

    Vyhozen IReactionHandler z PointsModule

--- src/Inkluzitron/Modules/Points/PointsModule.cs
+++ src/Inkluzitron/Modules/Points/PointsModule.cs
@@ -5,7 +5,6 @@ using Inkluzitron.Contracts;
 using Inkluzitron.Extensions;
 using Inkluzitron.Models.Settings;
 using Inkluzitron.Services;
-using Inkluzitron.Utilities;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -16,7 +15,7 @@ namespace Inkluzitron.Modules.Points
     [Alias("points")]
     [Summary("Body se počítají stejně jako u GrillBot. Za každou reakci uživatel obdrží 0 až 10 bodů, za zprávu 0 až 25 bodů. Po odeslání zprávy " +
         "bot počítá jedno minutový cooldown. U reakce je cooldown 30 vteřin.")]
-    public class PointsModule : ModuleBase, IReactionHandler
+    public class PointsModule : ModuleBase
     {
         private PointsService PointsService { get; }
         private DiscordSocketClient Client { get; }
@@ -92,60 +91,6 @@ namespace Inkluzitron.Modules.Points
                 await ReplyAsync("Tento uživatel nemá žádný záznam o bodech.");
             else
                 await GetLeaderboardAsync(pos);
-        }
-
-        public async Task<bool> HandleReactionAddedAsync(IUserMessage message, IEmote reaction, IUser user)
-        {
-            var embed = message.Embeds.FirstOrDefault();
-            if (embed == null || embed.Author == null || embed.Footer == null)
-                return false; // Embed checks
-
-            if (!ReactionSettings.PaginationReactions.Any(emote => emote.IsEqual(reaction)))
-                return false; // Reaction check.
-
-            if (message.ReferencedMessage == null)
-                return false;
-
-            if (!embed.TryParseMetadata<PointsEmbedMetadata>(out var metadata))
-                return false; // Not a points board embed.
-
-            var count = await PointsService.GetUserCount();
-            if (count == 0)
-                return false;
-
-            int newStart = metadata.Start;
-            if (reaction.IsEqual(ReactionSettings.MoveToFirst))
-                newStart = 0;
-            else if (reaction.IsEqual(ReactionSettings.MoveToLast))
-                newStart = count - 1;
-            else if (reaction.IsEqual(ReactionSettings.MoveToNext))
-                newStart += BoardPageLimit;
-            else if (reaction.IsEqual(ReactionSettings.MoveToPrevious))
-                newStart -= BoardPageLimit;
-
-            if (newStart >= count)
-                newStart = count - 1;
-            else if (newStart < 0)
-                newStart = 0;
-
-            newStart -= newStart % BoardPageLimit;
-
-            var context = new CommandContext(Client, message.ReferencedMessage);
-
-            if (newStart != metadata.Start)
-            {
-                var board = PointsService.GetLeaderboard(newStart, BoardPageLimit);
-
-                var newEmbed = new PointsEmbed()
-                    .WithBoard(board, Client, context.Client.CurrentUser, count, newStart, BoardPageLimit)
-                    .Build();
-
-                await message.ModifyAsync(msg => msg.Embed = newEmbed);
-            }
-
-            if (!context.IsPrivate) // DMs have blocked removing reactions.
-                await message.RemoveReactionAsync(reaction, user);
-            return true;
         }
     }
 }
---
+++ src/Inkluzitron/Modules/Points/PointsReactionHandler.cs
@@ -0,0 +1,82 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Inkluzitron.Contracts;
+using Inkluzitron.Extensions;
+using Inkluzitron.Models.Settings;
+using Inkluzitron.Services;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Inkluzitron.Modules.Points
+{
+    public class PointsReactionHandler : IReactionHandler
+    {
+        private PointsService PointsService { get; }
+        private DiscordSocketClient Client { get; }
+        private ReactionSettings ReactionSettings { get; }
+        private readonly int BoardPageLimit = 10;
+
+        public PointsReactionHandler(PointsService pointsService, DiscordSocketClient client,
+            ReactionSettings reactionSettings)
+        {
+            PointsService = pointsService;
+            Client = client;
+            ReactionSettings = reactionSettings;
+        }
+
+        public async Task<bool> HandleReactionAddedAsync(IUserMessage message, IEmote reaction, IUser user)
+        {
+            var embed = message.Embeds.FirstOrDefault();
+            if (embed == null || embed.Author == null || embed.Footer == null)
+                return false; // Embed checks
+
+            if (!ReactionSettings.PaginationReactions.Any(emote => emote.IsEqual(reaction)))
+                return false; // Reaction check.
+
+            if (message.ReferencedMessage == null)
+                return false;
+
+            if (!embed.TryParseMetadata<PointsEmbedMetadata>(out var metadata))
+                return false; // Not a points board embed.
+
+            var count = await PointsService.GetUserCount();
+            if (count == 0)
+                return false;
+
+            int newStart = metadata.Start;
+            if (reaction.IsEqual(ReactionSettings.MoveToFirst))
+                newStart = 0;
+            else if (reaction.IsEqual(ReactionSettings.MoveToLast))
+                newStart = count - 1;
+            else if (reaction.IsEqual(ReactionSettings.MoveToNext))
+                newStart += BoardPageLimit;
+            else if (reaction.IsEqual(ReactionSettings.MoveToPrevious))
+                newStart -= BoardPageLimit;
+
+            if (newStart >= count)
+                newStart = count - 1;
+            else if (newStart < 0)
+                newStart = 0;
+
+            newStart -= newStart % BoardPageLimit;
+
+            var context = new CommandContext(Client, message.ReferencedMessage);
+
+            if (newStart != metadata.Start)
+            {
+                var board = PointsService.GetLeaderboard(newStart, BoardPageLimit);
+
+                var newEmbed = new PointsEmbed()
+                    .WithBoard(board, Client, context.Client.CurrentUser, count, newStart, BoardPageLimit)
+                    .Build();
+
+                await message.ModifyAsync(msg => msg.Embed = newEmbed);
+            }
+
+            if (!context.IsPrivate) // DMs have blocked removing reactions.
+                await message.RemoveReactionAsync(reaction, user);
+            return true;
+        }
+    }
+}
