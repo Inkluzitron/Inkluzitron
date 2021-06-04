970a2d84e0765f5f4b41bf2b6202d4fa2723109f
Jan Chaloupka <osobni@janchaloupka.cz>
2021-06-05 01:00:11 +0200

    Zapracovány vyžádané změny

--- src/Inkluzitron/Extensions/BotDatabaseContextExtensions.cs
+++ src/Inkluzitron/Extensions/BotDatabaseContextExtensions.cs
@@ -2,6 +2,7 @@
 using Inkluzitron.Data;
 using Inkluzitron.Data.Entities;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,6 +14,9 @@ namespace Inkluzitron.Extensions
         // TODO: Fix properly
         static public async Task<User> GetUserEntityAsync(this BotDatabaseContext context, IUser user)
         {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+
             var displayName = user.GetDisplayName();
             var userEntity = await context.Users.AsQueryable()
                 .FirstOrDefaultAsync(o => o.Id == user.Id);
@@ -31,6 +35,9 @@ namespace Inkluzitron.Extensions
         // TODO: Fix properly
         static public async Task<User> GetOrCreateUserEntityAsync(this BotDatabaseContext context, IUser user)
         {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+
             var userEntity = await context.GetUserEntityAsync(user);
 
             if (userEntity != null)
