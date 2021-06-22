d6f4a2c015e74fbf90cd2d4c875768d9bcc78e46
Crackonosh <lukashais@getgoogleoff.me>
2021-06-22 18:05:19 +0200

    Add files for invite module

---
+++ src/Inkluzitron/Handlers/InviteHandler.cs
@@ -0,0 +1,74 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Discord.WebSocket;
+using Inkluzitron.Data;
+using Inkluzitron.Extensions;
+using Inkluzitron.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace Inkluzitron.Handlers
+{
+    /// <summary>
+    /// Handler to catch event on joining new user
+    /// </summary>
+    public class InviteHandler : IHandler
+    {
+        private DiscordSocketClient DiscordSocketClient { get; }
+
+        private DatabaseFactory DatabaseFactory { get; }
+        private UsersService UsersService { get; }
+
+        public InviteHandler(DiscordSocketClient discordSocketClient,
+            DatabaseFactory databaseFactory,
+            UsersService usersService)
+        {
+            DiscordSocketClient = discordSocketClient;
+            DatabaseFactory = databaseFactory;
+            UsersService = usersService;
+
+            DiscordSocketClient.UserJoined += OnUserJoinedAsync;
+        }
+
+        /// <summary>
+        /// Checks which invite link has been used by newly joined user and
+        /// updates corresponding record in database for it
+        /// </summary>
+        /// <param name="user">newly joined user</param>
+        private async Task OnUserJoinedAsync(SocketGuildUser user)
+        {
+            var dbContext = DatabaseFactory.Create();
+            var inviteLinks = await user.Guild.GetInvitesAsync();
+            var inviteeDb = await dbContext.GetOrCreateUserEntityAsync(user);
+
+            foreach (var link in inviteLinks)
+            {
+                if (link.Inviter.Id != DiscordSocketClient.CurrentUser.Id) continue;
+                if (link.Uses == 0) continue;
+
+                var invite = await dbContext.Invites.AsQueryable()
+                    .Where(i => i.InviteLink == link.Url)
+                    .FirstAsync();
+
+                if (invite == null) continue;
+
+                dbContext.Invites.Update(invite).Entity.UsedByUserId = inviteeDb.Id;
+                await dbContext.SaveChangesAsync();
+
+                await link.DeleteAsync();
+                await dbContext.DisposeAsync();
+
+                await user.Guild.DefaultChannel.SendMessageAsync(
+                    "Vítej ***" +
+                    user.Username +
+                    "*** na našem serveru!");
+                return;
+            }
+
+            await user.Guild.DefaultChannel.SendMessageAsync(
+                "Nový uživatel " +
+                user.Username +
+                " se připojil na server pomocí odkazu, který nebyl vytvořen `$invite` příkazem!");
+            await dbContext.DisposeAsync();
+        }
+    }
+}
---
+++ src/Inkluzitron/Migrations/20210622133307_Invites.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Inkluzitron.Migrations
+{
+    public partial class Invites : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Invites",
+                columns: table => new
+                {
+                    InviteLink = table.Column<string>(type: "TEXT", nullable: false),
+                    GeneratedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    GeneratedByUserId = table.Column<ulong>(type: "INTEGER", nullable: false),
+                    UsedByUserId = table.Column<ulong>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Invites", x => x.InviteLink);
+                    table.ForeignKey(
+                        name: "FK_Invites_Users_GeneratedByUserId",
+                        column: x => x.GeneratedByUserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Invites_Users_UsedByUserId",
+                        column: x => x.UsedByUserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Invites_GeneratedByUserId",
+                table: "Invites",
+                column: "GeneratedByUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Invites_UsedByUserId",
+                table: "Invites",
+                column: "UsedByUserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Invites");
+        }
+    }
+}
