fbdebdb5f015c8787e621aaa893a30a57addcb09
Jan Chaloupka <osobni@janchaloupka.cz>
2021-06-24 01:53:07 +0200

    Výpis chybových zpráv do Discord roomky (closes #94)

---
+++ src/Inkluzitron/Services/ChannelLogger/ChannelLoggerExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Configuration;
+using System;
+
+namespace Inkluzitron.Services.ChannelLogger
+{
+    static public class ChannelLoggerExtensions
+    {
+        static public ILoggingBuilder AddChannelLogger(
+        this ILoggingBuilder builder)
+        {
+            builder.AddConfiguration();
+
+            builder.Services.TryAddEnumerable(
+                ServiceDescriptor.Singleton<ILoggerProvider, ChannelLoggerProvider>());
+
+            LoggerProviderOptions.RegisterProviderOptions
+                <ChannelLoggerConfiguration, ChannelLoggerProvider>(builder.Services);
+
+            return builder;
+        }
+
+        static public ILoggingBuilder AddChannelLogger(
+            this ILoggingBuilder builder,
+            Action<ChannelLoggerConfiguration> configure)
+        {
+            builder.AddChannelLogger();
+            builder.Services.Configure(configure);
+
+            return builder;
+        }
+    }
+}
