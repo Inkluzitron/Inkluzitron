348810af5724122bbde85ea90f18987b77011824
Jan Chaloupka <osobni@janchaloupka.cz>
2021-06-24 01:53:07 +0200

    Výpis chybových zpráv do Discord roomky (closes #94)

--- src/Inkluzitron/Models/Settings/BotSettings.cs
+++ src/Inkluzitron/Models/Settings/BotSettings.cs
@@ -6,10 +6,12 @@ namespace Inkluzitron.Models.Settings
     {
         public BotSettings(IConfiguration configuration)
         {
+            LoggingChannelId = configuration.GetValue<ulong>(nameof(LoggingChannelId));
             HomeGuildId = configuration.GetValue<ulong>(nameof(HomeGuildId));
             PointsKarmaIncrement = configuration.GetValue<int>(nameof(PointsKarmaIncrement));
         }
 
+        public ulong LoggingChannelId { get; }
         public ulong HomeGuildId { get; }
         public int PointsKarmaIncrement { get; }
     }
---
+++ src/Inkluzitron/Services/ChannelLogger/ChannelLoggerProvider.cs
@@ -0,0 +1,61 @@
+﻿using Discord;
+using Discord.WebSocket;
+using Inkluzitron.Models.Settings;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+namespace Inkluzitron.Services.ChannelLogger
+{
+    public class ChannelLoggerProvider : ILoggerProvider
+    {
+        private readonly IDisposable OnChangeToken;
+        private readonly ConcurrentDictionary<string, ChannelLogger> Loggers = new();
+
+        private ChannelLoggerConfiguration CurrentConfig { get; set; }
+        private DiscordSocketClient DiscordClient { get; }
+        private BotSettings BotSettings { get; }
+        private ITextChannel LoggingChannel { get; set; } = null;
+
+        public ChannelLoggerProvider(
+            IOptionsMonitor<ChannelLoggerConfiguration> config, DiscordSocketClient discordClient, BotSettings botSettings)
+        {
+            CurrentConfig = config.CurrentValue;
+            OnChangeToken = config.OnChange(updatedConfig => CurrentConfig = updatedConfig);
+            DiscordClient = discordClient;
+            BotSettings = botSettings;
+
+            DiscordClient.Ready += DiscordClientReady;
+        }
+
+        private Task DiscordClientReady()
+        {
+            var guild = DiscordClient.GetGuild(BotSettings.HomeGuildId);
+            if (guild != null)
+            {
+                LoggingChannel = guild.GetTextChannel(BotSettings.LoggingChannelId);
+                foreach (var logger in Loggers)
+                {
+                    logger.Value.LoggingChannel = LoggingChannel;
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public ILogger CreateLogger(string categoryName) =>
+            Loggers.GetOrAdd(categoryName, name => new ChannelLogger(name, GetCurrentConfig, LoggingChannel));
+
+        private ChannelLoggerConfiguration GetCurrentConfig() => CurrentConfig;
+
+        public void Dispose()
+        {
+            Loggers.Clear();
+            OnChangeToken.Dispose();
+
+            GC.SuppressFinalize(this);
+        }
+    }
+}
