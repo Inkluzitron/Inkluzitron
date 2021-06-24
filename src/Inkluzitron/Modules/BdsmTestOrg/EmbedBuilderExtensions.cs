35494a745632c8405cc51763dd86819eb70bd03f
Jan Chaloupka <osobni@janchaloupka.cz>
2021-06-24 02:32:01 +0200

    Zjendodušení testu - jen jeden výsledek na uživatele. Closes #78

--- src/Inkluzitron/Modules/BdsmTestOrg/EmbedBuilderExtensions.cs
+++ src/Inkluzitron/Modules/BdsmTestOrg/EmbedBuilderExtensions.cs
@@ -11,37 +11,23 @@ namespace Inkluzitron.Modules.BdsmTestOrg
     {
         static public EmbedBuilder WithBdsmTestOrgQuizInvitation(this EmbedBuilder builder, BdsmTestOrgSettings settings, IUser user)
         {
-            builder.WithTitle(settings.TestLinkUrl);
-            builder.WithColor(new Color(0, 0, 0));
-            builder.WithUrl(settings.TestLinkUrl);
-            builder.WithDescription(settings.NoResultsOnRecordMessage);
-            builder.WithFooter("0/0");
-            builder.WithMetadata(new QuizEmbedMetadata
-            {
-                ResultId = 0,
-                UserId = user.Id,
-                PageNumber = 0
-            });
-
-            return builder;
+            return builder.WithTitle(settings.TestLinkUrl)
+                .WithColor(new Color(0, 0, 0))
+                .WithUrl(settings.TestLinkUrl)
+                .WithDescription(settings.NoResultsOnRecordMessage)
+                .WithFooter("BDSMTest.org");
         }
 
-        static public EmbedBuilder WithBdsmTestOrgQuizResult(this EmbedBuilder builder, BdsmTestOrgSettings settings, BdsmTestOrgResult quizResult, int pageNumber, int pageCount)
+        static public EmbedBuilder WithBdsmTestOrgQuizResult(this EmbedBuilder builder, BdsmTestOrgSettings settings, BdsmTestOrgResult quizResult)
         {
             if (quizResult is null)
                 throw new ArgumentNullException(nameof(quizResult));
 
-            builder.WithTitle(quizResult.Link);
-            builder.WithColor(new Color(0, 0, 0));
-            builder.WithUrl(quizResult.Link);
-            builder.WithTimestamp(quizResult.SubmittedAt);
-            builder.WithFooter($"{pageNumber}/{pageCount}");
-            builder.WithMetadata(new QuizEmbedMetadata
-            {
-                ResultId = quizResult.Id,
-                UserId = quizResult.UserId,
-                PageNumber = pageNumber
-            });
+            builder.WithTitle(quizResult.Link)
+                .WithColor(new Color(0, 0, 0))
+                .WithUrl(quizResult.Link)
+                .WithTimestamp(quizResult.SubmittedAt)
+                .WithFooter("BDSMTest.org");
 
             var relevantItems = quizResult.Items
                 .Where(i => i.Score >= settings.StrongTraitThreshold)
