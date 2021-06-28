ea3e283f2a903fd679b172beca75c90525f5432d
Michal Halabica <m.halabica@gmail.com>
2021-06-28 07:20:01 +0200

    Doplnění extension metod

---
+++ src/Inkluzitron/Extensions/DiscordClientExtensions.cs
@@ -0,0 +1,50 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrillBot.App.Extensions.Discord
+{
+    static public class DiscordClientExtensions
+    {
+        static public async Task<IUser> FindUserAsync(this BaseSocketClient client, string username, string discriminator)
+        {
+            var user = client.GetUser(username, discriminator);
+
+            if (user != null)
+                return user;
+
+            foreach (var guild in client.Guilds)
+            {
+                await guild.DownloadUsersAsync();
+                user = guild.Users
+                    .FirstOrDefault(o => o.Username == username && (string.IsNullOrEmpty(discriminator) || o.Discriminator == discriminator));
+
+                if (user != null)
+                    break;
+            }
+
+            return user;
+        }
+
+        static public async Task<IUser> FindUserAsync(this BaseSocketClient client, ulong id)
+        {
+            var user = client.GetUser(id);
+
+            if (user != null)
+                return user;
+
+            foreach (var guild in client.Guilds)
+            {
+                await guild.DownloadUsersAsync();
+
+                user = guild.GetUser(id);
+
+                if (user != null)
+                    return user;
+            }
+
+            return await client.Rest.GetUserAsync(id);
+        }
+    }
+}
