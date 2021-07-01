f71be9f603297e18390b1d437764758531d4d5c8
Crackonosh <lukashais@getgoogleoff.me>
2021-07-01 15:45:27 +0200

    make hobit happy

--- src/Inkluzitron/Modules/HuggingModule.cs
+++ src/Inkluzitron/Modules/HuggingModule.cs
@@ -20,9 +20,9 @@ namespace Inkluzitron.Modules
 
         [Command("hug")]
         [Summary("Hugne sebe nebo všechny uživatele, kteří jsou označení ve zprávě.")]
-        public async Task HuggingAsync([Name("koho")] params IUser[] user)
+        public async Task HuggingAsync([Name("koho")] params IUser[] users)
         {
-            if (user.Length == 0)
+            if (users.Length == 0)
             {
                 var userName = await UsersService.GetDisplayNameAsync(Context.User);
                 await Context.Channel.SendMessageAsync(
@@ -31,9 +31,9 @@ namespace Inkluzitron.Modules
                 return;
             }
 
-            foreach (var u in user)
+            foreach (var user in users)
             {
-                var userName = await UsersService.GetDisplayNameAsync(u);
+                var userName = await UsersService.GetDisplayNameAsync(user);
                 await Context.Channel.SendMessageAsync(
                     $"{Config["Hugging"]} **{Format.Sanitize(userName)}**"
                 );
