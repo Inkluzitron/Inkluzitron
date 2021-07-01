5c017a0a649fed27e0e6b4b7acfbba4e2efbe2e5
Crackonosh <lukashais@getgoogleoff.me>
2021-07-01 13:12:54 +0200

    change help message for PointsModule

--- src/Inkluzitron/Modules/Points/PointsModule.cs
+++ src/Inkluzitron/Modules/Points/PointsModule.cs
@@ -15,8 +15,9 @@ namespace Inkluzitron.Modules.Points
     [Group("body")]
     [Name("Body")]
     [Alias("points")]
-    [Summary("Body se počítají stejně jako u GrillBot. Za každou reakci uživatel obdrží 0 až 10 bodů, za zprávu 0 až 25 bodů. Po odeslání zprávy " +
-        "bot počítá jedno minutový cooldown. U reakce je cooldown 30 vteřin.")]
+    [Summary("Body se počítají podobně jako u GrillBot. Za každou zprávu uživatel obdrží 0 až 25 bodů, " +
+             "za reakci uživatel obdrží 0 až 10 bodů a autorovi zprávy přidá 5 bodů. Po odeslání zprávy " +
+             "bot počítá jedno minutový cooldown. U reakce je cooldown 30 vteřin.")]
     public class PointsModule : ModuleBase
     {
         private PointsService PointsService { get; }
