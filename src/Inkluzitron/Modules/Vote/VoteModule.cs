0414606724769f040d29a695059ab47971026731
Khub <khub@hurrdurr.cz>
2021-07-15 20:09:56 +0200

    Adds flex to vote module help summary

--- src/Inkluzitron/Modules/Vote/VoteModule.cs
+++ src/Inkluzitron/Modules/Vote/VoteModule.cs
@@ -11,7 +11,13 @@ namespace Inkluzitron.Modules
     [Summary(ModuleSummary)]
     public class VoteModule : ModuleBase
     {
-        public const string ModuleSummary = "**Ukázková zadání:**\r\n" + Example1 + "\r\n" + Example2 + "\r\n" + Example3 + "\r\n" + Example4 + "\r\n" + Example5;
+        public const string ModuleSummary = "Zprávu s příkazem hlasování je možné upravovat a měnit tak parametry hlasování i poté, co už začalo nebo skončilo.\r\n\r\n"
+            + "**Ukázková zadání:**\r\n"
+            + Example1 + "\r\n"
+            + Example2 + "\r\n"
+            + Example3 + "\r\n"
+            + Example4 + "\r\n"
+            + Example5;
 
         public const string Example1 = "$vote Otázka hlasování s pevným ukončením\r\n" +
             ":fire: Ohýnek\r\n" +
