54ec9e4fcd9eae670ec9239f1fe284aa8af9ec57
Khub <khub@hurrdurr.cz>
2021-12-29 19:11:12 +0100

    Properly handle birthday date to allow for 29th Feb birthdays (closes #150)

---
+++ src/Inkluzitron/Migrations/20211229172306_UpdateUnsetBirthdayYear.cs
@@ -0,0 +1,24 @@
+﻿using Inkluzitron.Data.Entities;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Inkluzitron.Migrations
+{
+    public partial class UpdateUnsetBirthdayYear : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            if (!migrationBuilder.IsSqlite())
+                return;
+
+            migrationBuilder.Sql(
+                "UPDATE Users " +
+                $"SET BirthdayDate = '{User.UnsetBirthdayYear}' || strftime('-%m-%d', BirthdayDate) || ' 00:00:00' " +
+                $"WHERE BirthdayDate IS NOT NULL AND strftime('%Y', BirthdayDate)='{User.LegacyUnsetBirthdayYear}';"
+            );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
--- src/Inkluzitron/Services/TypeReaders/DateWithOptionalYearTypeReader.cs
+++ src/Inkluzitron/Services/TypeReaders/DateWithOptionalYearTypeReader.cs
@@ -1,4 +1,5 @@
 ﻿using Discord.Commands;
+using Inkluzitron.Data.Entities;
 using System;
 using System.Globalization;
 using System.Text.RegularExpressions;
@@ -8,7 +9,9 @@ namespace Inkluzitron.Services.TypeReaders
 {
     public class DateWithOptionalYearTypeReader : TypeReader
     {
-        static public readonly int FallbackYear = DateTime.MinValue.Year;
+        static public readonly int FallbackYear = User.UnsetBirthdayYear;
+        static private readonly Regex DateMonthPattern = new(@"^([1-9]|(?:1[0-9])|(?:2[0-9])|(?:3[01]))(?:\.|/)([1-9]|(?:1[0-2]))(?:\.|/)$");
+        static private readonly Regex DateMonthYearPattern = new(@"^([1-9]|(?:1[0-9])|(?:2[0-9])|(?:3[01]))(?:\.|/)([1-9]|(?:1[0-2]))(?:\.|/)(\d{4})$");
 
         public override Task<TypeReaderResult> ReadAsync(ICommandContext context, string input, IServiceProvider services)
         {
@@ -16,22 +19,37 @@ namespace Inkluzitron.Services.TypeReaders
             var culture = CultureInfo.InvariantCulture;
 
             input = Regex.Replace(input, "\\s+", string.Empty);
-            DateTime? result = null;
+            var reassembledDate = string.Empty;
 
-            if (DateTime.TryParseExact(input, "d/M", culture, styles, out var dateWithoutYear))
-                result = new DateTime(FallbackYear, dateWithoutYear.Month, dateWithoutYear.Day, 0, 0, 0, DateTimeKind.Local);
-            else if (DateTime.TryParseExact(input, "d'/'M'/'yyyy", culture, styles, out var ordinarilySeparatedDate))
-                result = ordinarilySeparatedDate;
-            else if (DateTime.TryParseExact(input, "d'.'M'.'", culture, styles, out var dotSeparatedDateWithoutYear))
-                result = new DateTime(FallbackYear, dotSeparatedDateWithoutYear.Month, dotSeparatedDateWithoutYear.Day, 0, 0, 0, DateTimeKind.Local);
-            else if (DateTime.TryParseExact(input, "d'.'M'.'yyyy", culture, styles, out var dotSeparatedDate))
-                result = dotSeparatedDate;
+            if (DateMonthYearPattern.Match(input) is Match m1 && m1.Success)
+            {
+                var day = int.Parse(m1.Groups[1].Value);
+                var month = int.Parse(m1.Groups[2].Value);
+                var year = int.Parse(m1.Groups[3].Value);
+                reassembledDate = $"{year:D4}-{month:D2}-{day:D2}T00:00:00";
+            }
+            else if (DateMonthPattern.Match(input) is Match m2 && m2.Success)
+            {
+                var day = int.Parse(m2.Groups[1].Value);
+                var month = int.Parse(m2.Groups[2].Value);
+                reassembledDate = $"{FallbackYear:D4}-{month:D2}-{day:D2}T00:00:00";
+            }
 
-            return Task.FromResult(
-                result is DateTime successfullyParsedDate
-                ? TypeReaderResult.FromSuccess(successfullyParsedDate)
-                : TypeReaderResult.FromError(CommandError.ParseFailed, $"Nelze přečíst datum '{input}'.")
-            );
+            TypeReaderResult result;
+            if (DateTime.TryParseExact(reassembledDate, "s", culture, styles, out var reconstructedDate))
+            {
+                result = TypeReaderResult.FromSuccess(new DateTime(
+                    reconstructedDate.Year, reconstructedDate.Month, reconstructedDate.Day,
+                    0, 0, 0,
+                    DateTimeKind.Local
+                ));
+            }
+            else
+            {
+                result = TypeReaderResult.FromError(CommandError.ParseFailed, $"Nelze přečíst datum '{input}'.");
+            }
+
+            return Task.FromResult(result);
         }
     }
 }
