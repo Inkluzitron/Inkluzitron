d6f26c72ad60b394f749a8e64684868911a825a0
Jan Chaloupka <osobni@janchaloupka.cz>
2022-08-02 23:07:10 +0200

    Přidána možnost whitelistu roomek pro anonymní odesílání zpráv

--- src/Inkluzitron/Models/Settings/SendSettings.cs
+++ src/Inkluzitron/Models/Settings/SendSettings.cs
@@ -13,10 +13,18 @@ namespace Inkluzitron.Models.Settings
             ErrorGuildNotFound = section.GetRequired<string>(nameof(ErrorGuildNotFound));
             ErrorRoomNotFound = section.GetRequired<string>(nameof(ErrorRoomNotFound));
             ErrorNoContent = section.GetRequired<string>(nameof(ErrorNoContent));
+            ErrorRoomNotWhitelisted = section.GetRequired<string>(nameof(ErrorRoomNotWhitelisted));
+            ListMessage = section.GetRequired<string>(nameof(ListMessage));
+            RoomWhitelist = section.GetRequired<ulong[]>(nameof(RoomWhitelist));
+            WhitelistEnabled = section.GetRequired<bool>(nameof(WhitelistEnabled));
         }
 
         public string ErrorGuildNotFound { get; }
         public string ErrorRoomNotFound { get; }
         public string ErrorNoContent { get; }
+        public string ErrorRoomNotWhitelisted { get; }
+        public string ListMessage { get; }
+        public ulong[] RoomWhitelist { get; }
+        public bool WhitelistEnabled { get; }
     }
 }
--- src/Inkluzitron/Modules/SendModule.cs
+++ src/Inkluzitron/Modules/SendModule.cs
@@ -5,11 +5,14 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
+using Discord.WebSocket;
 using Inkluzitron.Models.Settings;
+using System.Collections.Generic;
 
 namespace Inkluzitron.Modules
 {
     [Name("Anonymní odesílání zpráv")]
+    [Summary("Zprávy je možné anonymně odesílat i přes DMs s botem.")]
     public class SendModule : ModuleBase
     {
         private BotSettings BotSettings { get; }
@@ -21,10 +24,46 @@ namespace Inkluzitron.Modules
             SendSettings = sendSettings;
         }
 
+        private string ListWhitelistedRooms(SocketGuild guild)
+        {
+            if (!SendSettings.WhitelistEnabled)
+            {
+                return "všech místností kde můžeš normálně psát";
+            }
+
+            var channels = SendSettings.RoomWhitelist
+                .Select(id => guild.TextChannels.FirstOrDefault(c => c.Id == id))
+                .Where(c => c is not null)
+                .Select(c => c.Mention)
+                .ToArray();
+
+            return string.Join(", ", channels);
+        }
+
+        [Command("send list")]
+        [Summary("Vypíše seznam roomek, kde je možné posílat anonymní zprávy.")]
+        public async Task ListAsync()
+        {
+            var guild = Context.Client.GetGuild(BotSettings.HomeGuildId);
+            if (guild is null)
+            {
+                await ReplyAsync(SendSettings.ErrorGuildNotFound);
+                return;
+            }
+
+            await ReplyAsync(string.Format(SendSettings.ListMessage, ListWhitelistedRooms(guild)));
+        }
+
         [Command("send")]
         [Summary("Odešle anonymně zprávu (včetně příloh) do zadané roomky.")]
         public async Task SendAsync([Name("cílová roomka")] string roomName, [Remainder][Name("zpráva")] string messageText = null)
         {
+            if(roomName == "list") // Protože list příkaz
+            {
+                await ListAsync();
+                return;
+            }
+
             var guild = Context.Client.GetGuild(BotSettings.HomeGuildId);
             if (guild is null)
             {
@@ -50,6 +89,19 @@ namespace Inkluzitron.Modules
         [Summary("Odešle anonymně zprávu (včetně příloh) do zadané roomky.")]
         public async Task SendAsync([Name("#cílová roomka")] IMessageChannel channel, [Remainder][Name("zpráva")] string messageText = null)
         {
+            if (SendSettings.WhitelistEnabled && !SendSettings.RoomWhitelist.Contains(channel.Id))
+            {
+                var guild = Context.Client.GetGuild(BotSettings.HomeGuildId);
+                if (guild is null)
+                {
+                    await ReplyAsync(SendSettings.ErrorGuildNotFound);
+                    return;
+                }
+
+                await ReplyAsync(string.Format(SendSettings.ErrorRoomNotWhitelisted, ListWhitelistedRooms(guild)));
+                return;
+            }
+
             var attachments = Context.Message.Attachments;
             if (messageText == null && attachments.Count == 0)
             {
